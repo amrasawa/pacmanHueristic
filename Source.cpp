0927bbe161f881ef0732598787649c9d13f54015
amrasawa <karmelsalma5@gmail.com>
2024-10-14 20:59:42 +0300

    code is complete

--- Source.cpp
+++ Source.cpp
@@ -1,28 +1,17 @@
 #include <iostream>
 #include<vector>
-#include<pair>
-using namespace std; 
-
-int pacmanHueristicValue(int state[11][20]) {
-	int hueristicValue;
-	int huerisitcPill, hueristicGhost;
-	
-	return hueristicValue;
-}
+#include<cmath>
+using namespace std;
+pair<int, int>pacmanPosition = { 7,5 };
 
 int hueristicPill(int state[11][20]) {
 	int numberOfPills = 0;
-	for (int i = 0; i < 11; ++i) {
+	for (int i = 0; i <   11; ++i) {
 		for (int j = 0; j < 20; ++j)
 			if (state[i][j] == 2)
 				numberOfPills++;
 	}
 	return numberOfPills;
-}
-
-int hueristicGhost(int state[11][20]) {
-	vector<pair<int, int>> ghostPosition;
-	detectGhost(ghostPosition);
 }
 
 void detectGhost(vector<pair<int, int>>& ghosts, int state[11][20]) {
@@ -33,6 +22,43 @@ void detectGhost(vector<pair<int, int>>& ghosts, int state[11][20]) {
 		}
 	}
 }
-int main() {
 
+int hueristicGhost(int state[11][20]) {
+	vector<pair<int, int>> ghostPosition;
+	detectGhost(ghostPosition,state);
+	int minDistance = INT_MAX;
+	double distance;
+	for (int i = 0; i < ghostPosition.size(); ++i) {
+		distance = sqrt(pow(pacmanPosition.first - ghostPosition[i].first,2) + pow(pacmanPosition.second - ghostPosition[i].second,2));
+		if (distance < minDistance)
+			minDistance = (int)distance;
+	}
+	return minDistance;
+	return 0;
+}
+int pacmanHueristicValue(int state[11][20]) {
+	int hueristicValue;
+	int pill, ghost;
+	pill = hueristicPill(state);
+	ghost = hueristicGhost(state);
+
+	hueristicValue = 23 - ghost + pill;//23 is the max distance from a ghost and pacman
+	return hueristicValue;
+}
+
+int main() {
+	int state[11][20] = {
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,3},
+	{1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1},
+	{1,0,1,1,0,1,0,1,1,1,1,1,1,0,1,0,1,1,0,3},
+	{1,0,1,2,0,0,0,2,2,2,2,2,2,0,2,0,0,1,0,1},
+	{1,0,1,2,1,1,2,1,1,1,0,1,1,0,1,1,0,1,0,1},
+	{1,0,2,2,2,2,2,1,0,0,0,0,1,0,0,0,0,0,0,1},
+	{1,0,1,2,1,1,2,1,1,1,1,1,1,2,1,1,0,1,2,1},
+	{1,0,1,2,0,4,2,2,2,2,2,2,2,0,0,0,0,1,2,1},
+	{1,0,1,1,0,1,2,1,1,1,1,1,1,0,1,2,1,1,2,1},
+	{1,0,0,0,0,1,2,2,2,0,0,0,0,0,1,2,2,2,2,1},
+	{1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}
+	};
+	cout << pacmanHueristicValue(state);
 }
